4328e8a6659afcab2f5e2fdd84dbab5aecbf3813
kisumutalent <homiemusa@gmail.com>
2018-02-26 10:02:58 +0300

    adding checking for a ValueError to be raised

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,10 +1,17 @@
 import unittest
 from app.calculator import Calculator
+
 class TddInPythonExample(unittest.TestCase):
+     def setUp(self):
+          self.calc = Calculator()
+     
      def test_calculator_add_method_returns_correct_result(self):
-          calc = Calculator()
+          #calc = Calculator()
           result = calc.add(2,2)
           self.assertEqual(4, result)
+          
+     def test_calculator_returns_error_message_if_both_args_not_numbers(self):
+          self.assertRaises(ValueError, self.calc.add, 'two', 'three')
           
 if __name__ == '__main__':
      unittest.main()
