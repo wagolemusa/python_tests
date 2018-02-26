4bf93a53ea4750f00c6e969a132b4c8768516dd3
kisumutalent <homiemusa@gmail.com>
2018-02-26 10:30:19 +0300

    passing all tests colloct

--- app/calculator.py
+++ app/calculator.py
@@ -1,3 +1,8 @@
 class Calculator(object):
      def add(self, x, y):
-          return x+y
+          number_types = (int, long, float, complex)
+
+          if isinstance(x, number_types) and isinstance(y, number_types):
+               return x + y
+          else:
+               raise ValueError
